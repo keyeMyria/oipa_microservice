698e4137f9b80b219199579eb5162b776b615fef
Taufik Hidayat <taufik@zimmermanzimmerman.nl>
2018-08-20 23:16:23 +0200

    Added country to activity endpoint

--- oipa_graphql/activity.py
+++ oipa_graphql/activity.py
@@ -1,11 +1,12 @@
 import graphene
 from django_filters import FilterSet, CharFilter
-from graphene import relay, ObjectType
+from graphene import relay
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
-from oipa_db.iati.models import Activity, Title
+from oipa_db.iati.models import Activity, Title, ActivityRecipientCountry
 from oipa_db.iati.transaction.models import Transaction
+from oipa_db.geodata.models import Country
 
 
 class TransactionNode(DjangoObjectType):
@@ -14,9 +15,37 @@ class TransactionNode(DjangoObjectType):
         model = Transaction
 
 
+class CountryNode(DjangoObjectType):
+    polygon = graphene.String()
+    center_longlat = graphene.String()
+
+    class Meta:
+        model = Country
+        exclude_fields = (
+            'center_longlat',
+        )
+
+    def resolve_polygon(self, context, **kwargs):
+        return self.polygon
+
+    def resolve_center_longlat(self, context, **kwargs):
+        return self.center_longlat.json
+
+
+class ActivityRecipientCountryNode(DjangoObjectType):
+    country = graphene.List(CountryNode)
+
+    class Meta:
+        model = ActivityRecipientCountry
+
+    def resolve_country(self, context, **kwargs):
+        return Country.objects.filter(code=self.country_id)
+
+
 class ActivityNode(DjangoObjectType):
     title = graphene.String()
     transactions = graphene.List(TransactionNode)
+    recipient_country = graphene.List(ActivityRecipientCountryNode)
 
     class Meta:
         model = Activity
@@ -36,6 +65,9 @@ class ActivityNode(DjangoObjectType):
 
     def resolve_transactions(self, context, **kwargs):
         return Transaction.objects.filter(activity_id=self.id)
+
+    def resolve_recipient_country(self, context, **kwargs):
+        return ActivityRecipientCountry.objects.filter(activity_id=self.id)
 
 
 class ActivityFilter(FilterSet):
