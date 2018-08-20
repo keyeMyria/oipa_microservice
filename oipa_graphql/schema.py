2c8cdaf39e6adf22eedf4cf898c0def3e5c886ec
Taufik Hidayat <taufik@zimmermanzimmerman.nl>
2018-08-20 02:37:48 +0200

    Added transactions to the activity node

--- oipa_graphql/schema.py
+++ oipa_graphql/schema.py
@@ -3,40 +3,44 @@ from graphene import relay, ObjectType
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
-from oipa_db.iati.models import Activity as ActivityModel, Title as TitleModel
+from oipa_db.iati.models import Activity, Title
+from oipa_db.iati.transaction.models import Transaction
 
 
-class Title(DjangoObjectType):
+class TransactionNode(DjangoObjectType):
 
     class Meta:
-        model = TitleModel
-        interfaces = (relay.Node, )
+        model = Transaction
 
 
-class Activity(DjangoObjectType):
+class ActivityNode(DjangoObjectType):
     title = graphene.String()
+    transactions = graphene.List(TransactionNode)
 
     class Meta:
-        model = ActivityModel
+        model = Activity
         filter_fields = ['iati_identifier', ]
         interfaces = (relay.Node, )
 
     def resolve_title(self, args, context, info):
         try:
-            narrative = TitleModel.objects.get(
+            narrative = Title.objects.get(
                 activity_id=self.id).narratives.first()
 
             return narrative.content if narrative else ''
 
-        except TitleModel.DoesNotExist:
+        except Title.DoesNotExist:
             pass
 
         return ''
 
+    def resolve_transactions(self, args, context, info):
+        return Transaction.objects.filter(activity_id=self.id)
+
 
 class Query(ObjectType):
-    activity = relay.Node.Field(Activity)
-    activities = DjangoFilterConnectionField(Activity)
+    activity = relay.Node.Field(ActivityNode)
+    activities = DjangoFilterConnectionField(ActivityNode)
 
 
 schema = graphene.Schema(query=Query)
