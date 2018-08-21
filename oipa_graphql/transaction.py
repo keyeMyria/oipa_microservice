e4e264557e79116098db97ab1fb8a7ac78f88c15
Taufik Hidayat <taufik@zimmermanzimmerman.nl>
2018-08-21 23:55:01 +0200

    Added utils module

--- oipa_graphql/transaction.py
+++ oipa_graphql/transaction.py
@@ -1,13 +1,10 @@
 import graphene
-
-from django.db.models import FloatField, Sum
+from django.db.models import Sum
 from graphene import relay, String, List
 from graphene_django import DjangoObjectType
-from graphene_django.filter import DjangoFilterConnectionField
-
 from django_filters import FilterSet
-
 from oipa_db.iati.transaction.models import Transaction
+from oipa_graphql.utils import OrderedDjangoFilterConnectionField
 
 
 class TransactionListNode(DjangoObjectType):
@@ -24,30 +21,6 @@ class TransactionListFilter(FilterSet):
         fields = {
             'ref': ['exact', ],
         }
-
-
-class OrderedDjangoFilterConnectionField(DjangoFilterConnectionField):
-
-    @classmethod
-    def connection_resolver(
-            cls, resolver, connection, default_manager, max_limit,
-            enforce_first_or_last, filterset_class, filtering_args,
-            root, info, **args):
-
-        filter_kwargs = {k: v for k, v in args.items() if k in filtering_args}
-        qs = default_manager.get_queryset() \
-            if hasattr(default_manager, 'get_queryset') else default_manager
-
-        qs = filterset_class(data=filter_kwargs, queryset=qs).qs
-        order = args.get('orderBy', None)
-
-        if order:
-            qs = qs.order_by(*order)
-
-        return super(DjangoFilterConnectionField, cls).connection_resolver(
-            resolver, connection, qs, max_limit,
-            enforce_first_or_last, root, info, **args
-        )
 
 
 class TransactionSummaryNode(graphene.ObjectType):
---
+++ oipa_graphql/utils.py
@@ -0,0 +1,25 @@
+from graphene_django.filter import DjangoFilterConnectionField
+
+
+class OrderedDjangoFilterConnectionField(DjangoFilterConnectionField):
+
+    @classmethod
+    def connection_resolver(
+            cls, resolver, connection, default_manager, max_limit,
+            enforce_first_or_last, filterset_class, filtering_args,
+            root, info, **args):
+
+        filter_kwargs = {k: v for k, v in args.items() if k in filtering_args}
+        qs = default_manager.get_queryset() \
+            if hasattr(default_manager, 'get_queryset') else default_manager
+
+        qs = filterset_class(data=filter_kwargs, queryset=qs).qs
+        order = args.get('orderBy', None)
+
+        if order:
+            qs = qs.order_by(*order)
+
+        return super(DjangoFilterConnectionField, cls).connection_resolver(
+            resolver, connection, qs, max_limit,
+            enforce_first_or_last, root, info, **args
+        )
