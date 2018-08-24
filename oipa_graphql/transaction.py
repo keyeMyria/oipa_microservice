67756782c692ef6461a4464dcb33c7b128014c65
Taufik Hidayat <taufik@zimmermanzimmerman.nl>
2018-08-24 20:16:46 +0200

    Made an object orientaried for the summary node

--- oipa_graphql/transaction.py
+++ oipa_graphql/transaction.py
@@ -1,10 +1,9 @@
 import graphene
-from django.db.models import Sum
 from graphene import relay, String, List
 from graphene_django import DjangoObjectType
 from django_filters import FilterSet
 from oipa_db.iati.transaction.models import Transaction
-from oipa_graphql.utils import OrderedDjangoFilterConnectionField
+from oipa_graphql.utils import OrderedDjangoFilterConnectionField, SummaryNode
 
 
 class TransactionListNode(DjangoObjectType):
@@ -20,61 +19,17 @@ class TransactionListFilter(FilterSet):
             'ref': ['exact', ], }
 
 
-class TransactionSummaryNode(graphene.ObjectType):
+class TransactionSummaryNode(SummaryNode):
     value = graphene.Float()
     recipientCountryCode = graphene.String()
     recipientCountryName = graphene.String()
 
+    Model = Transaction
     FIELDS_MAPPING = {
         'recipientCountryCode': 'activity__recipient_country__code',
         'recipientCountryName': 'activity__recipient_country__name'}
-
     FIELDS_FILTER_MAPPING = {
         'recipientCountryCodeIn': 'activity__recipient_country__code__in', }
-
-    class Meta:
-        interfaces = (relay.Node, )
-
-    def get_group_by(self, context, **kwargs):
-        return [self.FIELDS_MAPPING.get(field) for field in kwargs['groupBy']]
-
-    def get_order_by(self, context, **kwargs):
-        return [('-' if '-' == field[0] else '') + self.FIELDS_MAPPING
-                .get(field.replace('-', '')) for field in kwargs['orderBy']]
-
-    def get_filters(self, context, **kwargs):
-        filters = {}
-        for field, filter_field in self.FIELDS_FILTER_MAPPING.items():
-            value = kwargs.get(field)
-            if value:
-                filters[filter_field] = value
-
-        return filters
-
-    def get_results(self, context, **kwargs):
-        filters = self.get_filters(context, **kwargs)
-        groups = self.get_group_by(context, **kwargs)
-        orders = self.get_order_by(context, **kwargs)
-        aggregation = kwargs['aggregation']
-        return Transaction.objects.values(*groups).annotate(
-            **{field: Sum(field) for field in aggregation}
-        ).order_by(*orders).filter(**filters)
-
-    def get_node_summaries(self, context, **kwargs):
-        results = self.get_results(context, **kwargs)
-        nodes = []
-        aggregation = kwargs['aggregation']
-        for result in results:
-            node = TransactionSummaryNode(**{field: result[
-                self.FIELDS_MAPPING.get(
-                    field)] for field in kwargs['groupBy']})
-
-            for field in aggregation:
-                setattr(node, field, result[field])
-
-            nodes.append(node)
-
-        return nodes
 
 
 class Query(object):
@@ -92,4 +47,4 @@ class Query(object):
         recipientCountryCodeIn=List(of_type=String))
 
     def resolve_transaction_summaries(self, context, **kwargs):
-        return TransactionSummaryNode().get_node_summaries(context, **kwargs)
+        return TransactionSummaryNode().get_nodes(context, **kwargs)
--- oipa_graphql/utils.py
+++ oipa_graphql/utils.py
@@ -1,4 +1,8 @@
+import graphene
+from graphene import relay
 from graphene_django.filter import DjangoFilterConnectionField
+from django.db import models
+from django.db.models import Sum
 
 
 class OrderedDjangoFilterConnectionField(DjangoFilterConnectionField):
@@ -24,3 +28,52 @@ class OrderedDjangoFilterConnectionField(DjangoFilterConnectionField):
             enforce_first_or_last, root, info, **args
         )
 
+
+class SummaryNode(graphene.ObjectType):
+    FIELDS_MAPPING = {}
+    FIELDS_FILTER_MAPPING = {}
+    Model = models.Model
+
+    class Meta:
+        interfaces = (relay.Node, )
+
+    def get_group_by(self, context, **kwargs):
+        return [self.FIELDS_MAPPING.get(field) for field in kwargs['groupBy']]
+
+    def get_order_by(self, context, **kwargs):
+        return [('-' if '-' == field[0] else '') + self.FIELDS_MAPPING
+                .get(field.replace('-', '')) for field in kwargs['orderBy']]
+
+    def get_filters(self, context, **kwargs):
+        filters = {}
+        for field, filter_field in self.FIELDS_FILTER_MAPPING.items():
+            value = kwargs.get(field)
+            if value:
+                filters[filter_field] = value
+
+        return filters
+
+    def get_results(self, context, **kwargs):
+        filters = self.get_filters(context, **kwargs)
+        groups = self.get_group_by(context, **kwargs)
+        orders = self.get_order_by(context, **kwargs)
+
+        return self.Model.objects.values(*groups).annotate(
+            **{field: Sum(field) for field in kwargs['aggregation']}
+        ).order_by(*orders).filter(**filters)
+
+    def get_nodes(self, context, **kwargs):
+        results = self.get_results(context, **kwargs)
+        nodes = []
+        aggregation = kwargs['aggregation']
+        for result in results:
+            node = self.__class__(**{field: result[
+                self.FIELDS_MAPPING.get(
+                    field)] for field in kwargs['groupBy']})
+
+            for field in aggregation:
+                setattr(node, field, result[field])
+
+            nodes.append(node)
+
+        return nodes
